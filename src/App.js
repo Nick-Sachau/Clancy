ac0839e9472584a54a56ee55aea3353186b815c9
Jadyn Calhoun <Jcalho217@west-mec.local>
2021-08-30 14:25:38 -0700

    router working

--- src/App.js
+++ src/App.js
@@ -2,24 +2,25 @@ import React, { useState, useEffect } from "react";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
 import Services from "./Pages/Services";
-import Navbar from './Components/Navbar';
-import Footer from './Components/Footer'
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
 import { Route, Switch } from "react-router-dom";
-
 
 function App() {
   return (
     <>
       <Navbar>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/about">
-          <About />
-        </Route>
-        <Route exact path="/services">
-          <Services />
-        </Route>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+          <Route exact path="/services">
+            <Services />
+          </Route>
+        </Switch>
       </Navbar>
       <Footer />
     </>
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from "react";
+import Home from "../Pages/Home";
+import About from "../Pages/About";
+import Services from "../Pages/Services";
+import { Route, Switch } from "react-router-dom";
 
 const Footer = () => {
-    return (
-        <div className="container">
-            
-        </div>
-    )
-}
+  return (
+    <>
+      <div className="contact container">
+        <h3>Contact Me</h3>
+        <h5>Clancy Briggs</h5>
+        <h5>+16234745069</h5>
+        <button>Call</button>
+      </div>
+      <div className="footer container">
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
 
-export default Footer
+          <Route path="/about">
+            <About />
+          </Route>
+        </Switch>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
