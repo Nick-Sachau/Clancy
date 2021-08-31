dd9880fe238a1185f26d0558cd41507ad9725197
Jadyn Calhoun <Jcalho217@west-mec.local>
2021-08-31 13:46:52 -0700

    pulling

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,18 +5,12 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <footer>
-      <div className="contactContainer">
-        <h3>Contact Me</h3>
-        <h5>Clancy Briggs</h5>
-        <h5>+16234745069</h5>
-        <button>Call!</button>
-      </div>
+      <h3>Contact Me</h3>
       <div className="footerContainer">
         {links
           .filter((link) => link.text !== "Error")
           .map((link) => {
             const { id, url, text } = link;
-
             return (
               <li key={id}>
                 <Link to={url}>{text}</Link>
@@ -24,11 +18,16 @@ const Footer = () => {
             );
           })}
       </div>
-      <div className="addressContainer">
-        <h3>Address</h3>
-        <h4>24710 W. Dove Trail, Buckeye, AZ 85326</h4>
+      <div className="contactContainer">
+        
+        <h5>Clancy Briggs</h5>
+        <h5>+16234745069</h5>
+        <button>Call!</button>
       </div>
-      
+      <div className="addressContainer">
+        <h4>Address</h4>
+        <h5>24710 W. Dove Trail, Buckeye, AZ 85326</h5>
+      </div>
     </footer>
   );
 };
