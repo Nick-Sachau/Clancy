daa7f9229ca4770faef81690816a0407570a3fec
Jadyn Calhoun <Jcalho217@west-mec.local>
2021-08-31 13:14:35 -0700

    all footer content up to date

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,19 +1,21 @@
 import React from "react";
-import { links } from '../util/consts';
-import { Link } from 'react-router-dom';
+import { links } from "../util/consts";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
-    return (
-        <footer>
-            <div className="contactContainer">
-                <h3>Contact Me</h3>
-                <h5>Clancy Briggs</h5>
-                <h5>+16234745069</h5>
-                <button>Call!</button>
-            </div>
-            <div className="footerContainer">
-                {links.filter((link) => link.text !== "Error").map((link) => {
-                    const { id, url, text } = link;
+  return (
+    <footer>
+      <div className="contactContainer">
+        <h3>Contact Me</h3>
+        <h5>Clancy Briggs</h5>
+        <h5>+16234745069</h5>
+        <button>Call!</button>
+      </div>
+      <div className="footerContainer">
+        {links
+          .filter((link) => link.text !== "Error")
+          .map((link) => {
+            const { id, url, text } = link;
 
             return (
               <li key={id}>
@@ -21,8 +23,12 @@ const Footer = () => {
               </li>
             );
           })}
-          
       </div>
+      <div className="addressContainer">
+        <h3>Address</h3>
+        <h4>24710 W. Dove Trail, Buckeye, AZ 85326</h4>
+      </div>
+      
     </footer>
   );
 };
