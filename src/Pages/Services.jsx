ea729019136c7f609ef063ca6af5d9028c5c91e1
Jadyn Calhoun <Jcalho217@west-mec.local>
2021-08-31 14:06:09 -0700

    basic content in progress

--- src/Pages/Services.jsx
+++ src/Pages/Services.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React from "react";
 
 const Services = () => {
-    return (
-        <div>
-            Services Page
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>Services</h1>
+      <img src="" alt="logo" />
+      <h3>Repair and Maintenance</h3>
+      <h4>
+        Cupidatat sunt anim qui do veniam. Laborum ut anim et qui. Ad voluptate
+        quis id aliquip nisi aute ex. Ut proident nulla cillum cupidatat. Quis
+        voluptate ullamco non deserunt dolor eu et laborum voluptate officia
+        pariatur est.
+      </h4>
+      <h3>AC Repair</h3>
+      <h4>
+        Cupidatat sunt anim qui do veniam. Laborum ut anim et qui. Ad voluptate
+        quis id aliquip nisi aute ex. Ut proident nulla cillum cupidatat. Quis
+        voluptate ullamco non deserunt dolor eu et laborum voluptate officia
+        pariatur est.
+      </h4>
+      <h3>RV Inspection</h3>
+      <h4>
+        Cupidatat sunt anim qui do veniam. Laborum ut anim et qui. Ad voluptate
+        quis id aliquip nisi aute ex. Ut proident nulla cillum cupidatat. Quis
+        voluptate ullamco non deserunt dolor eu et laborum voluptate officia
+        pariatur est.
+      </h4>
+    </div>
+  );
+};
 
-export default Services
+export default Services;
